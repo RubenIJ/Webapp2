bf7bf8227ab8104f1470da59acf31fcc23960f0f
ThankY0uu <Ty_tonjes@hotmail.com>
2025-05-13 12:04:56 +0200

    push van header en footer

--- html/index.php
+++ html/index.php
@@ -1,3 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,11 +11,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/styling.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Hoofdpagina</title>
+    <link rel="stylesheet" href="css/styling.css">
 </head>
 <body>
-<?php require_once("frontend/header.php") ?>
+
+<?php require_once("frontend/header.php"); ?>
+
+<div class="bigboy">Bigboy is zichtbaar!</div>
+
+<?php require_once("frontend/footer.php"); ?>
 
 </body>
 </html>
