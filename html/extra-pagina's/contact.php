cfc5aa506a2bb0827590fbb6fcafc377eb1f6a6f
SadDisplay <1212236@student.roc-nijmegen.nl>
2025-05-14 11:55:24 +0200

    contact pagina verbeterd
    pagina verbeterd en css verplaatst

--- html/extra-pagina's/contact.php
+++ html/extra-pagina's/contact.php
@@ -5,6 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/ruben.css">
+    <link rel="stylesheet" href="../css/styling.css">
+
     <title>Contact</title>
 </head>
 <body>
@@ -18,5 +21,9 @@
         <button type="Submit" name="Submit">Verstuur</button>
     </form>
 </main>
+<footer>
+    <?php require_once("../components/footer.php") ?>
+
+</footer>
 </body>
 </html>
