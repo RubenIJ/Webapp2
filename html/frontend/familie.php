eff5298aa636fb817967dc463be8ddc83c6d7a3a
xingmyg <1213099@student.roc-nijmegen.nl>
2025-05-14 11:34:15 +0200

    Update familie.php

--- html/frontend/familie.php
+++ html/frontend/familie.php
@@ -9,6 +9,11 @@
     <title>Document</title>
 </head>
 <body>
-
+<div id="groot-blok">
+    <div class="blokjes">
+        <h1>Tekst iver hotel</h1>
+        <img src="html/fotos/"
+    </div>
+</div>
 </body>
 </html>
