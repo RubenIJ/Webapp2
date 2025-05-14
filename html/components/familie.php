d84901a1b04a9f3b87433916a9fed4410d81dd81
xingmyg <1213099@student.roc-nijmegen.nl>
2025-05-14 13:46:11 +0200

    familie pagina aanpassen
    nieuwe dingen toegevoegd

--- html/components/familie.php
+++ html/components/familie.php
@@ -1,19 +1,32 @@
 <!doctype html>
 <html lang="en">
-<link rel="stylesheet" href="xing.css">
 <head>
+    <link rel="stylesheet" href="../css/xing.css">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Familie pagina</title>
 </head>
 <body>
 <div id="groot-blok">
-    <div class="blokjes">
-        <h1>Tekst iver hotel</h1>
-        <img src="html/fotos/"
+    <div id="groot">
+         <div class="blok">
+            <h1>Voya Beach Resort</h1>
+             <h2>In het Bulgaarse Sveti Vlas ligt het luxe Excellent hotel Voya Beach Resort.
+                 Jong of oud, je komt de dagen heerlijk door.</h2>
+            <p> Stijlvolle en luxe inrichting
+                Culinaire verwennerij
+                Voor iedere leeftijd iets te doen
+                Maar liefst 5 glijbanen</p>
+             <img src=""
+         </div>
     </div>
 </div>
+
+
+
+
+
 </body>
 </html>
