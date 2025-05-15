3564272ed13a423ba33f58e2e19617dbf2fc1f91
xingmyg <1213099@student.roc-nijmegen.nl>
2025-05-15 09:45:15 +0200

    familie pagina afmaken
    Familie pagina opbouw en informatie afgemaakt

--- html/components/familie.php
+++ html/components/familie.php
@@ -9,24 +9,51 @@
     <title>Familie pagina</title>
 </head>
 <body>
-<div id="groot-blok">
-    <div id="groot">
-         <div class="blok">
-            <h1>Voya Beach Resort</h1>
-             <h2>In het Bulgaarse Sveti Vlas ligt het luxe Excellent hotel Voya Beach Resort.
-                 Jong of oud, je komt de dagen heerlijk door.</h2>
-            <p> Stijlvolle en luxe inrichting
-                Culinaire verwennerij
-                Voor iedere leeftijd iets te doen
-                Maar liefst 5 glijbanen</p>
-             <img src=""
-         </div>
+<div class="vakanties">
+<div class="vakantie-blok">
+    <div class="vakantie-inhoud">
+        <h1>Voya Beach Resort</h1>
+        <h2>In het Bulgaarse Sveti Vlas ligt het luxe Excellent hotel Voya Beach Resort.
+            Jong of oud, je komt de dagen heerlijk door.</h2>
+        <li>Stijlvolle en luxe inrichting</li>
+        <li>Culinaire verwennerij</li>
+        <li>Voor iedere leeftijd iets te doen</li>
+        <li>Maar liefst 5 glijbanen</li>
+        <button class="meer-info">meer informatie</button>
+    </div>
+    <div class="vakantie-fotos">
+        <img src="../fotos/vakantie-foto-1.png" alt="Voya Beach Resort">
     </div>
 </div>
-
-
-
-
-
+</div>
+<div class="vakanties">
+    <div class="vakantie-blok">
+        <div class="vakantie-inhoud">
+            <h1>Aparthotel Adagio Rome Vatican</h1>
+            <h2>
+                Bij Aparthotel Adagio Rome Vatican, net buiten het centrum van de Italiaanse stad Rome, zit je goed.</h2>
+            <p>Aparthotel Adagio Rome Vatican ligt in de wijk Balduina, ten noordwesten van het historische centrum van Rome.
+                In de directe omgeving zijn verschillende restaurants te vinden.</p>
+            <button class="meer-info">meer informatie</button>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../fotos/vakantie-foto-2.png" alt="Aparthotel Adagio Rome Vatican">
+        </div>
+    </div>
+</div>
+<div class="vakanties">
+    <div class="vakantie-blok">
+        <div class="vakantie-inhoud">
+            <h1>AP Cabanas Beach & Nature</h1>
+            <h2>Op zoek naar een luxe uitvalbasis in de Portugese Algarve? AP Cabanas Beach & Nature beschikt over o.a. een dakterras met zwembad, fitnessruimte en wellnesscentrum.</h2>
+            <p>AP Cabanas Beach & Nature beschikt over meerdere zwembaden, waaronder één op het dakterras.
+            Het uitzicht is hier prachtig! Neem een duik en relax vervolgens op een ligbedje.</p>
+            <button class="meer-info">meer informatie</button>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../fotos/vakantie-foto-3.png" alt="AP Cabanas Beach & Nature">
+        </div>
+    </div>
+</div>
 </body>
 </html>
