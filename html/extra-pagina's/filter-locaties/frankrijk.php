6824f4c4f18866cd9b9e00fdfb1b27c2fb5da154
xingmyg <1213099@student.roc-nijmegen.nl>
2025-06-16 14:37:28 +0200

    Locaties pagina's
    Alle html en css af voor deze pagina's

---
+++ html/extra-pagina's/filter-locaties/frankrijk.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Hoofdpagina</title>
+    <link rel="stylesheet" href="../../css/styling.css">
+    <link rel="stylesheet" href="../../css/xing.css">
+    <link rel="stylesheet" href="../../css/ruben.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+</head>
+<body>
+<header>
+    <?php require_once("../../components/header.php") ?>
+</header>
+<div class="naamgeving">
+    <h1>Populaire locaties</h1>
+</div>
+<div class="frankrijk ruimte">
+    <div class="frankrijk blok">
+        <div class="frankrijk inhoud">
+            <h1>Franse steden</h1>
+            <p>Franse steden ademen elegantie, geschiedenis en stijl.</p>
+            <p>In Parijs glinstert de Eiffeltoren boven brede boulevards, terwijl kunstenaars zich nestelen langs de
+                Seine.</p>
+            <p>Lyon charmeert met zijn oude straatjes en verfijnde gastronomie, en Marseille pulseert met Mediterrane
+                warmte,</p>
+            <p>kleurrijke markten en een mix van culturen.</p>
+            <p>Elke stad is als een levend museum vol kunst, architectuur en dat ongrijpbare dat Frankrijk zo
+                onweerstaanbaar maakt.</p>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../../fotos/frankrijk-stad.jpg" alt="frankrijk stad">
+        </div>
+    </div>
+</div>
+<div class="frankrijk ruimte">
+    <div class="blok-omgedraaid">
+        <div class="frankrijk inhoud">
+            <h1>Franse kust</h1>
+            <p>De Franse kust is puur poëzie in beweging.</p>
+            <p>Aan de Côte d’Azur schittert de zon op het azuurblauwe water, waar jachten glijden langs mondaine
+                badplaatsen als Nice en Cannes.</p>
+            <p>Verder naar het westen dansen surfers op de golven van Biarritz en in Normandië breken de golven
+                dramatisch tegen krijtrotsen onder grijze luchten vol mysterie.</p>
+            <p>Elke kustlijn heeft haar eigen ritme: chique, wild of rustig maar altijd doordrenkt van zeezout,</p>
+            <p>vrijheid en dat zachte Franse gevoel van leven zonder haast.</p>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../../fotos/frankrijk-kust.jpg" alt="frankrijk kust">
+        </div>
+    </div>
+</div>
+<div class="frankrijk ruimte">
+    <div class="frankrijk blok">
+        <div class="frankrijk inhoud">
+            <h1>Franse dorpen</h1>
+            <p>De Franse dorpjes zijn als scènes uit een dromerig schilderij.</p>
+            <p>Denk aan steegjes geplaveid met keien, lavendelvelden tot aan de horizon, en oude stenen huizen met
+                blauwgeverfde luiken.</p>
+            <p>In de dorpen van de Provence ruik je versgebakken brood en hoor je het zachte geklik van pétanque op het
+                dorpsplein.</p>
+            <p>Hier leeft men langzaam, met wijn in de hand en zon op de huid.</p>
+            <p>Elk dorp draagt een ziel vol charme, stilte en eeuwenoude verhalen die fluisteren door de muren.</p>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../../fotos/frankrijk-dorpje.jpg" alt="frankrijk dorp">
+        </div>
+    </div>
+</div>
+<footer>
+    <?php require_once("../../components/footer.php") ?>
+</footer>
+</body>
+</html>
---
+++ html/extra-pagina's/filter-locaties/spanje.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Hoofdpagina</title>
+    <link rel="stylesheet" href="../../css/styling.css">
+    <link rel="stylesheet" href="../../css/xing.css">
+    <link rel="stylesheet" href="../../css/ruben.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+</head>
+<body>
+<header>
+    <?php require_once("../../components/header.php") ?>
+</header>
+<div class="naamgeving">
+    <h1>Populaire locaties</h1>
+</div>
+<div class="spanje ruimte">
+    <div class="spanje blok">
+        <div class="spanje inhoud">
+            <h1>Spaanse steden</h1>
+            <p>De drukke steden van Spanje bruisen van leven, cultuur en eindeloze energie.</p>
+            <p>In Barcelona hoor je het geroezemoes van toeristen langs de Ramblas, terwijl in Madrid de pleinen gevuld
+                zijn met muziek, tapas en nachtelijke gesprekken.</p>
+            <p>Valencia danst tussen traditie en moderniteit, met festivals die de straten vullen met vuur en kleur.</p>
+            <p>Elke stad is een levend schilderij van hectiek, passie en Spaanse flair — een symfonie van mensen,
+                kleuren en geluiden die nooit echt stopt.</p>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../../fotos/spanje-stad.jpg" alt="spaanse stad">
+        </div>
+    </div>
+</div>
+<div class="spanje ruimte">
+    <div class="blok-omgedraaid">
+        <div class="spanje inhoud">
+            <h1>Spaanse kust</h1>
+            <p>Langs de kust van Spanje kussen gouden stranden het azuurblauwe water van de Middellandse Zee.</p>
+            <p>De zon straalt fel op de boulevards vol palmbomen, waar vissersbootjes deinen in de haven en de geur van
+                zeevruchten zich mengt met zout en zon.</p>
+            <p>Van de ruige kliffen van de Costa Brava tot de zachte golven van de Costa del Sol: de kust is een plek
+                van vrijheid, zomeravonden en eindeloze horizon.</p>
+            <p>Hier voelt elke ademhaling als vakantie.</p>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../../fotos/spanje-kust.jpg" alt="spaanse kust">
+        </div>
+    </div>
+</div>
+<div class="spanje ruimte">
+    <div class="spanje blok">
+        <div class="spanje inhoud">
+            <h1>Spaanse natuur</h1>
+            <p>De natuur van Spanje is ruig, veelzijdig en verrassend stil.</p>
+            <p>Van de besneeuwde toppen van de Pyreneeën tot de uitgestrekte vlaktes van La Mancha — elke regio heeft
+                zijn eigen ritme.</p>
+            <p>Wandelend door nationale parken als Doñana of Picos de Europa ontdek je wilde paarden, flamingo's en
+                diepe stilte tussen eeuwenoude bomen.</p>
+            <p>In Spanje is de natuur geen achtergrond, maar een levendig decor vol leven, kleur en contrast.</p>
+            <p>Een plek waar je ademhaling vertraagt en je je weer klein voelt onder een oneindige hemel.</p>
+        </div>
+        <div class="vakantie-fotos">
+            <img src="../../fotos/spanje-natuur.jpg" alt="spaanse natuur">
+        </div>
+    </div>
+</div>
+
+<footer>
+    <?php require_once("../../components/footer.php") ?>
+</footer>
+</body>
+</html>
